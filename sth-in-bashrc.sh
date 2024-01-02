1d2bd58266c73de7116d17777a0056af55e24b37
ToolploeA <Toolploe@163.com>
2024-01-02 15:32:32 +0800

    modified:   plot-cpptraj.in 	modified:   sth-in-bashrc.sh

--- sth-in-bashrc.sh
+++ sth-in-bashrc.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-
 # get .pdb file in RCSB with PDB ID
 # use: getpdb 2HAC
 function getpdb() {
     wget files.rcsb.org/download/$1.pdb
 }
+
+# top
+alias topu='top -u $USER -d 1 -c'
+
+# ls
+alias lt='ls -lth'
+alias ll='ls -l'
+
+# ambpdb in ambertools
+# use: pdb md.rs > md.pdb
+alias pdb='ambpdb -p prmtop -c '
