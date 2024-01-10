89c23ef55d2290629d10afcf99f56ae470c88cdd
ToolploeA <Toolploe@163.com>
2024-01-10 15:49:01 +0800

    modified:   ala_scan_prepare.py

--- ala_scan_prepare.py
+++ ala_scan_prepare.py
@@ -5,8 +5,6 @@
 
 # create prepare file for amberMD to perform ALA scan
 
-# still in development !!!
-
 import os
 from multiprocessing import Pool
 
@@ -17,16 +15,29 @@ def ala_scan_prepare(pdb_file, scan_list, leap_file = None, nprocs = None):
         os.mkdir(f'scan_{item}')
         cmd_line = f"cat {pdb_file} | awk '$5 != \"{item}\" || $3 == \"C\" || $3 == \"CA\" || $3 == \"N\"'"
         os.system(cmd_line)
+
+        # use python to replace res name to ALA, maybe could use shell cmd to run faster, (Yuhong LIN)
+        with open(f'scan_{item}', 'r') as f:
+            file = f.readlines()
+        for idx, line in enumerate(file):
+            if file[:4] == 'ATOM':
+                if line.strip().split()[4] == str(item):
+                    file[idx] = line[:17] + 'ALA' + line[20:]
+        with open(f'scan{item}', 'w') as f:
+            f.write(''.join(file))
+
         if leap_file:
             os.chidr(f'scan_{item}')
             os.system('tleap -f ../leap.in')
             os.chdir('../')
+
     if nprocs:
         pool = Pool(nprocs)
         for item in scan_list:
             pool.apply_async(func = perform_ala_scan_prepare, args = (item, leap_file))
         pool.close()
         pool.join()
+        
     else:
         for item in scan_list:
             perform_ala_scan_prepare(item, leap_file)
